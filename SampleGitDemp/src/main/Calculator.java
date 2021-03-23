5803864e7b02ddd2fa6f80fe6bb9bcc9210ab446
AGRIM <AGRIM@LAPTOP-8Q05IVB7>
2021-03-23 09:55:43 +0530

    added square method

--- SampleGitDemp/src/main/Calculator.java
+++ SampleGitDemp/src/main/Calculator.java
@@ -3,28 +3,25 @@ package main;
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		add(10, 20);
+	System.out.println("Hello World");
+  add(10,20);
 	}
-
-	public static void add(int num1, int num2) {
-
-		System.out.println(num1 + num2);
+	public static void add(int num1,int num2){
+		
+		System.out.println(num1+num2);
 	}
-
-	public static void diff(int num1, int num2) {
-
-		System.out.println(num1 + num2);
+	public static void diff(int num1,int num2){
+		
+		System.out.println(num1+num2);
 	}
-
-	public static void prod(int num1, int num2) {
-
-		System.out.println(num1 * num2);
+	public static void prod(int num1,int num2){
+		
+		System.out.println(num1*num2);
 	}
-
-	public static void square(int num1) {
-
-		System.out.println(num1 * num1);
+public static void square(int num1){
+		
+		System.out.println(num1*num1);
 	}
+	
 
 }
