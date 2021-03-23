281cd665847269bb771f4960eb69245bc4ed2c6f
AGRIM <AGRIM@LAPTOP-8Q05IVB7>
2021-03-23 10:11:10 +0530

    added div and mod method

--- SampleGitDemp/src/main/Calculator.java
+++ SampleGitDemp/src/main/Calculator.java
@@ -3,25 +3,33 @@ package main;
 public class Calculator {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
-  add(10,20);
+		System.out.println("Hello World");
+		add(10, 20);
 	}
-	public static void add(int num1,int num2){
-		
-		System.out.println(num1+num2);
+
+	public static void add(int num1, int num2) {
+
+		System.out.println(num1 + num2);
 	}
-	public static void diff(int num1,int num2){
-		
-		System.out.println(num1+num2);
+
+	public static void diff(int num1, int num2) {
+
+		System.out.println(num1 + num2);
 	}
-	public static void prod(int num1,int num2){
-		
-		System.out.println(num1*num2);
+
+	public static void prod(int num1, int num2) {
+
+		System.out.println(num1 * num2);
 	}
-public static void square(int num1){
-		
-		System.out.println(num1*num1);
+
+	public static void div(int num1, int num2) {
+
+		System.out.println(num1 / num2);
 	}
-	
+
+	public static void mod(int num1, int num2) {
+
+		System.out.println(num1 % num2);
+	}
 
 }
